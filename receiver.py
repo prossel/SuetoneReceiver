dab60ce563c1c588b7297375389c17ff94d8bf0d
Pierre Rossel <>
2024-05-05 16:01:29 +0200

    clean exit with ctrl + c

--- receiver.py
+++ receiver.py
@@ -17,37 +17,47 @@ def notification_handler(sender: int, data: bytearray):
         
 async def run():
     scanner = BleakScanner()
-    try:
-        while True:
-            devices = await scanner.discover()
+    while True:
+        devices = await scanner.discover()
 
+        for device in devices:
+            if device.name == device_name:
+                print(f"Found device with name {device.name} and address {device.address}")
+                try:
+                    async with BleakClient(device) as client:
+                        print(f"Connected: {client.is_connected}")
+                        await client.start_notify(characteristic_uuid, notification_handler)
+                        while True:
+                            if not client.is_connected:
+                                print("Disconnected, trying to reconnect...")
+                                break
+                            await asyncio.sleep(1.0)  # check connection status every second
+                        if client.is_connected:
+                            await client.stop_notify(characteristic_uuid)
+                except BleakError as e:
+                    print(f"Could not connect to device: {e}")
+                break
+        else:
+            print(f"No device found with the desired name {device_name}")
+            # print the list of devices found
+            print("Devices found:")
             for device in devices:
-                if device.name == device_name:
-                    print(f"Found device with name {device.name} and address {device.address}")
-                    try:
-                        async with BleakClient(device) as client:
-                            print(f"Connected: {client.is_connected}")
-                            await client.start_notify(characteristic_uuid, notification_handler)
-                            while True:
-                                if not client.is_connected:
-                                    print("Disconnected, trying to reconnect...")
-                                    break
-                                await asyncio.sleep(1.0)  # check connection status every second
-                            if client.is_connected:
-                                await client.stop_notify(characteristic_uuid)
-                    except BleakError as e:
-                        print(f"Could not connect to device: {e}")
-                    break
-            else:
-                print(f"No device found with the desired name {device_name}")
-                # print the list of devices found
-                print("Devices found:")
-                for device in devices:
-                    print(f" - {device.name}")
-                    
-                await asyncio.sleep(1.0)  # wait for a second before scanning again
-    except KeyboardInterrupt:
-        print("Received exit signal, stopping...")
+                print(f" - {device.name}")
+                
+            await asyncio.sleep(1.0)  # wait for a second before scanning again
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
+try:
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
+except KeyboardInterrupt:
+    print(" Received exit signal in loop, stopping...")
+     
+    # Cancel all tasks lingering
+    tasks = asyncio.all_tasks(loop=loop)
+    for task in tasks:
+        task.cancel()
+    # Gather all tasks and let them finish
+    loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
+finally:
+    # cleanup
+    loop.close()
